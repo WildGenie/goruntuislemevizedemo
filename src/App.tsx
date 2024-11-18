f074142352e4e2e87bfc1fcfe02c4bd9ed17b19f
hackemalll <hackemalll@gmail.com>
2024-11-18 04:42:39 +0000

    jsx'e çevrildi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AdaptiveThresholdingDemo from './components/AdaptiveThresholdingDemo'
 import MorphologicalDemo from './components/MorphologicalDemo'
 import MorphologicalOps from './components/MorphologicalOps'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
